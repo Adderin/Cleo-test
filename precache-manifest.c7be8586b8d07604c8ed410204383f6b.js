49a3fa84ecccd24bc4f9573614bd0b3e7499f18f
Adderin <adderin@gmail.com>
2020-02-17 10:50:14 +0100

    Updates

---
+++ precache-manifest.c7be8586b8d07604c8ed410204383f6b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e5406003e1b392ee6deab2a3cf7ff52",
+    "url": "/cleo-test/index.html"
+  },
+  {
+    "revision": "2391b8c56c59a71abf3b",
+    "url": "/cleo-test/static/css/main.d498bfb3.chunk.css"
+  },
+  {
+    "revision": "5013d577a5412386c329",
+    "url": "/cleo-test/static/js/2.c848ebd3.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/cleo-test/static/js/2.c848ebd3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2391b8c56c59a71abf3b",
+    "url": "/cleo-test/static/js/main.8b58dbab.chunk.js"
+  },
+  {
+    "revision": "b0c44b8bb9f268a5e1d7",
+    "url": "/cleo-test/static/js/runtime-main.4e567a5f.js"
+  }
+]);
